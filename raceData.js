a35d8716c31dd6d5883c23931a9b9e3b6e2994d5
airgiraffe <airgiraffe@users.noreply.github.com>
2022-01-19 10:42:10 +0000

    Fixed base skills

--- raceData.js
+++ raceData.js
@@ -13,24 +13,24 @@ class Race {
 
 let altmer = new Race('altmer');
 altmer.baseSkills = [ 
-    5,       //Alchemy
-    10,       //Alteration
-    5,       //Block
-    10,       //Conjuration
-    15,       //Destruction
-    10,       //Enchanting
-    5,       //Evasion
-    5,       //Heavy Armor
-    15,       //Illusion
-    5,       //Lockpicking
-    5,       //Marksman
-    5,       //One-handed
-    5,       //Pickpocket
-    10,       //Restoration
-    5,       //Smithing
-    5,       //Sneak
-    5,       //Speech
-    5];       //Two-handed
+    5,      //smithing
+    5,      //heavyarmor
+    5,      //block
+    5,      //twohanded
+    5,      //onehanded
+    5,      //marksman
+    5,      //evasion
+    5,      //sneak
+    5,      //lockpicking
+    5,      //pickpocket
+    5,      //speech
+    5,      //alchemy
+    15,      //illusion
+    10,      //conjuration
+    15,      //destruction
+    10,      //restoration
+    10,      //alteration
+    10];     //enchanting
 
 altmer.baseAttributes = [130, 90, 80];
 altmer.bonusAttributes = [50, 0, 0];
@@ -40,24 +40,25 @@ altmer.baseResists = [0, 75, 0];
 
 let bosmer = new Race('bosmer');
 bosmer.baseSkills = [
-    10,       //Alchemy
-    5,       //Alteration
-    5,       //Block
-    5,       //Conjuration
-    5,       //Destruction
-    5,       //Enchanting
-   15,       //Evasion
-    5,       //Heavy Armor
-    5,       //Illusion
-   10,       //Lockpicking
-   15,       //Marksman
-    5,       //One-handed
-   10,       //Pickpocket
-    5,       //Restoration
-    5,       //Smithing
-   10,       //Sneak
-    5,       //Speech
-    5];       //Two-handed
+    5,      //smithing
+    5,      //heavyarmor
+    5,      //block
+    5,      //twohanded
+    5,      //onehanded
+    15,      //marksman
+    15,      //evasion
+    10,      //sneak
+    10,      //lockpicking
+    10,      //pickpocket
+    5,      //speech
+    10,      //alchemy
+    5,      //illusion
+    5,      //conjuration
+    5,      //destruction
+    5,      //restoration
+    5,      //alteration
+    5];     //enchanting
+
 bosmer.baseAttributes = [110, 90, 120];
 bosmer.bonusAttributes = [0, 0, 0];
 bosmer.baseRegens = [1.05, 0, 1.65];
@@ -66,24 +67,24 @@ bosmer.baseResists = [0, 75, 0];
 
 let breton = new Race('breton');
 breton.baseSkills = [
-    10,       //Alchemy
-    10,       //Alteration
-     5,       //Block
-    20,       //Conjuration
-     5,       //Destruction
-     5,       //Enchanting
-     5,       //Evasion
-     5,       //Heavy Armor
-    10,       //Illusion
-     5,       //Lockpicking
-     5,       //Marksman
-     5,       //One-handed
-     5,       //Pickpocket
-    10,       //Restoration
-     5,       //Smithing
-     5,       //Sneak
-    10,       //Speech
-     5];       //Two-handed
+    5,      //smithing
+    5,      //heavyarmor
+    5,      //block
+    5,      //twohanded
+    5,      //onehanded
+    5,      //marksman
+    5,      //evasion
+    5,      //sneak
+    5,      //lockpicking
+    5,      //pickpocket
+    10,      //speech
+    10,      //alchemy
+    10,      //illusion
+    20,      //conjuration
+    5,      //destruction
+    10,      //restoration
+    10,      //alteration
+    5];     //enchanting
 breton.baseAttributes = [120, 90, 90];
 breton.bonusAttributes = [0, 0, 0];
 breton.baseRegens = [1.15, 0, 1.55];
@@ -92,24 +93,24 @@ breton.baseResists = [10, 0, 0];
 
 let dunmer = new Race('dunmer');
 dunmer.baseSkills = [
-    10,       //Alchemy
-    5,       //Alteration
-    5,       //Block
-    5,       //Conjuration
-   15,       //Destruction
-    5,       //Enchanting
-   10,       //Evasion
-    5,       //Heavy Armor
-   10,       //Illusion
-    5,       //Lockpicking
-    5,       //Marksman
-   15,       //One-handed
-    5,       //Pickpocket
-    5,       //Restoration
-    5,       //Smithing
-   10,       //Sneak
-    5,       //Speech
-    5];       //Two-handed
+    5,      //smithing
+    5,      //heavyarmor
+    5,      //block
+    5,      //twohanded
+    15,      //onehanded
+    5,      //marksman
+    10,      //evasion
+    10,      //sneak
+    5,      //lockpicking
+    5,      //pickpocket
+    5,      //speech
+    10,      //alchemy
+    10,      //illusion
+    5,      //conjuration
+    15,      //destruction
+    5,      //restoration
+    5,      //alteration
+    5];     //enchanting
 dunmer.baseAttributes = [100, 100, 100];
 dunmer.bonusAttributes = [0, 0, 0];
 dunmer.baseRegens = [1.1, 0, 1.6];
@@ -118,24 +119,25 @@ dunmer.baseResists = [0, 0, 0];
 
 let imperial = new Race('imperial');
 imperial.baseSkills = [ 
-    5,       //Alchemy
-    5,       //Alteration
-   10,       //Block
-    5,       //Conjuration
-   10,       //Destruction
-    5,       //Enchanting
-    5,       //Evasion
-   10,       //Heavy Armor
-    5,       //Illusion
-    5,       //Lockpicking
-    5,       //Marksman
-   10,       //One-handed
-    5,       //Pickpocket
-   15,       //Restoration
-    5,       //Smithing
-    5,       //Sneak
-   15,       //Speech
-    5];       //Two-handed
+    5,      //smithing
+    10,      //heavyarmor
+    10,     //block
+    5,      //twohanded
+    10,      //onehanded
+    5,      //marksman
+    5,      //evasion
+    5,      //sneak
+    5,      //lockpicking
+    5,      //pickpocket
+    15,      //speech
+    5,      //alchemy
+    5,      //illusion
+    5,      //conjuration
+    10,     //destruction
+    15,      //restoration
+    5,      //alteration
+    5];     //enchanting
+
 imperial.baseAttributes = [100, 110, 110];
 imperial.bonusAttributes = [0, 0, 30];
 imperial.baseRegens = [1.1, 0, 1.9];
@@ -145,24 +147,25 @@ imperial.baseResists = [0, 0, 0];
 
 let khajiit = new Race('khajiit');
 khajiit.baseSkills = [
-    10,       //Alchemy
-    5,       //Alteration
-    5,       //Block
-    5,       //Conjuration
-    5,       //Destruction
-    5,       //Enchanting
-    5,       //Evasion
-    5,       //Heavy Armor
-    5,       //Illusion
-   10,       //Lockpicking
-   10,       //Marksman
-   10,       //One-handed
-   10,       //Pickpocket
-    5,       //Restoration
-    5,       //Smithing
-   20,       //Sneak
-    5,       //Speech
-    5];       //Two-handed
+    5,      //smithing
+    5,      //heavyarmor
+    5,      //block
+    5,      //twohanded
+    10,      //onehanded
+    10,      //marksman
+    5,      //evasion
+    20,      //sneak
+    10,      //lockpicking
+    10,      //pickpocket
+    5,      //speech
+    10,      //alchemy
+    5,      //illusion
+    5,      //conjuration
+    5,      //destruction
+    5,      //restoration
+    5,      //alteration
+    5];     //enchanting
+
 khajiit.baseAttributes = [90, 90, 120];
 khajiit.bonusAttributes = [0, 0, 0];
 khajiit.baseRegens = [1, 0, 1.65];
@@ -171,24 +174,25 @@ khajiit.baseResists = [0, 0, 0];
 
 let nord = new Race('nord');
 nord.baseSkills = [
-    5,       //Alchemy
-    5,       //Alteration
-    10,       //Block
-    5,       //Conjuration
-    5,       //Destruction
-    5,       //Enchanting
-    10,       //Evasion
-    10,       //Heavy Armor
-    5,       //Illusion
-    5,       //Lockpicking
-    5,       //Marksman
-    10,       //One-handed
-    5,       //Pickpocket
-    5,       //Restoration
-    15,       //Smithing
-    5,       //Sneak
-    5,       //Speech
-    15];       //Two-handed
+    15,      //smithing
+    10,      //heavyarmor
+    10,      //block
+    15,      //twohanded
+    10,      //onehanded
+    5,      //marksman
+    10,      //evasion
+    5,      //sneak
+    5,      //lockpicking
+    5,      //pickpocket
+    5,      //speech
+    5,      //alchemy
+    5,      //illusion
+    5,      //conjuration
+    5,      //destruction
+    5,      //restoration
+    5,      //alteration
+    5];     //enchanting
+
 nord.baseAttributes = [80, 120, 120];
 nord.bonusAttributes = [0, 0, 0];
 nord.baseRegens = [1.05, 0, 1.65];
@@ -197,24 +201,25 @@ nord.baseResists = [0, 0, 0];
 
 let orismer = new Race('orismer');
 orismer.baseSkills = [
-    5,       //Alchemy
-    5,       //Alteration
-    10,       //Block
-    5,       //Conjuration
-    5,       //Destruction
-    5,       //Enchanting
-    10,       //Evasion
-    15,       //Heavy Armor
-    5,       //Illusion
-    5,       //Lockpicking
-    5,       //Marksman
-    10,       //One-handed
-    5,       //Pickpocket
-    5,       //Restoration
-    15,       //Smithing
-    5,       //Sneak
-    5,       //Speech
-    10];       //Two-handed
+    15,      //smithing
+    15,      //heavyarmor
+    10,      //block
+    10,      //twohanded
+    10,      //onehanded
+    5,      //marksman
+    10,      //evasion
+    5,      //sneak
+    5,      //lockpicking
+    5,      //pickpocket
+    5,      //speech
+    5,      //alchemy
+    5,      //illusion
+    5,      //conjuration
+    5,      //destruction
+    5,      //restoration
+    5,      //alteration
+    5];     //enchanting
+    
 orismer.baseAttributes = [60, 130, 110];
 orismer.bonusAttributes = [0, 0, 0];
 orismer.baseRegens = [1, 0, 1.7];
@@ -223,24 +228,25 @@ orismer.baseResists = [5, 0, 0];
 
 let redguard = new Race('redguard');
 redguard.baseSkills = [
-    5,       //Alchemy
-    10,       //Alteration
-    15,       //Block
-    5,       //Conjuration
-    10,       //Destruction
-    5,       //Enchanting
-    5,       //Evasion
-    5,       //Heavy Armor
-    5,       //Illusion
-    5,       //Lockpicking
-    10,       //Marksman
-    15,       //One-handed
-    5,       //Pickpocket
-    5,       //Restoration
-    10,       //Smithing
-    5,       //Sneak
-    5,       //Speech
-    5];       //Two-handed
+    10,      //smithing
+    5,      //heavyarmor
+    15,      //block
+    5,      //twohanded
+    15,      //onehanded
+    10,      //marksman
+    5,      //evasion
+    5,      //sneak
+    5,      //lockpicking
+    5,      //pickpocket
+    5,      //speech
+    5,      //alchemy
+    5,      //illusion
+    5,      //conjuration
+    10,      //destruction
+    5,      //restoration
+    10,      //alteration
+    5];     //enchanting
+
 redguard.baseAttributes = [70, 110, 120];
 redguard.bonusAttributes = [0, 0, 0];
 redguard.baseRegens = [1, 0, 1.65];
@@ -250,24 +256,25 @@ redguard.baseResists = [0, 75, 75];
 
 let saxheel = new Race('saxheel');
 saxheel.baseSkills = [ 
-    5,       //Alchemy
-    10,       //Alteration
-    5,       //Block
-    5,       //Conjuration
-    5,       //Destruction
-    5,       //Enchanting
-    15,       //Evasion
-    5,       //Heavy Armor
-    5,       //Illusion
-    15,       //Lockpicking
-    5,       //Marksman
-    5,       //One-handed
-    10,       //Pickpocket
-    10,       //Restoration
-    5,       //Smithing
-    10,       //Sneak
-    5,       //Speech
-    5];       //Two-handed
+    5,      //smithing
+    5,      //heavyarmor
+    5,      //block
+    5,      //twohanded
+    5,      //onehanded
+    5,      //marksman
+    15,      //evasion
+    10,      //sneak
+    15,      //lockpicking
+    10,      //pickpocket
+    5,      //speech
+    5,      //alchemy
+    5,      //illusion
+    5,      //conjuration
+    5,      //destruction
+    10,      //restoration
+    10,      //alteration
+    5];     //enchanting
+    
 saxheel.baseAttributes = [90, 105, 120];
 saxheel.bonusAttributes = [0, 0, 0];
 saxheel.baseRegens = [1.1, 0, 1.6];
